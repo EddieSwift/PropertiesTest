53ff7eee4bb8feee00838fec7b4b08c6fdb2a4e4
Eddie <Eddie@galchenko.local>
2018-02-10 20:55:16 +0200

    Initial Commit

---
+++ PropertiesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PropertiesTest
+//
+//  Created by Eddie on 10.02.2018.
+//  Copyright © 2018 Eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PropertiesTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PropertiesTest
+//
+//  Created by Eddie on 10.02.2018.
+//  Copyright © 2018 Eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
